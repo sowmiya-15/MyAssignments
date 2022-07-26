a3e81348416946f16fc891967cc9823379c11718
sowmiya-15 <110019686+sowmiya-15@users.noreply.github.com>
2022-07-26 22:29:23 +0530

    Add files via upload

---
+++ swaping.java
@@ -0,0 +1,16 @@
+
+public class swaping {
+	public static void main(String[] args) {
+		 int a= 10;
+		 int b= 20;
+		 int temp;
+		 temp =a;
+		 a=b;
+		 b=temp;
+		 System.out.println("the value of a :"+ a );
+		 System.out.println("the value of b :"+ b);
+		 
+			
+	}
+
+}
