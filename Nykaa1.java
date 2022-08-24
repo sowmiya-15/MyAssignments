8ebcfb4e6f9df8ed5a6f9dcb0926f4e13e6ba0dc
sowmiya-15 <110019686+sowmiya-15@users.noreply.github.com>
2022-08-24 22:06:27 +0530

    Add files via upload

---
+++ Nykaa1.java
@@ -0,0 +1,29 @@
+package week7;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Nykaa1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.nykaa.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		WebElement Brands = driver.findElement(By.xpath("//a[text()='brands']"));
+		Actions builder=new Actions(driver);
+		builder.moveToElement(Brands).perform();
+	driver.findElement(By.xpath("class=\"css-ov2o3v\""));
+	
+
+	}
+
+}
